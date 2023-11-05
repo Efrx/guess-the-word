2ea4f42ac8f693170d6c6170263d12b4b75a860b
Efrx <efrainrood@gmail.com>
2023-11-05 01:04:11 -0600

    add winner status

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-// // import { useState } from 'react'
 import { Children, useEffect, useState } from 'react'
 import './App.css'
 
@@ -80,15 +79,37 @@ const WORDS_LIST = [
 ]
 
 const Input = ({ word, updateWordBoard, updateFailList, failTries}) => {
+  const checkIsFail = (array,inValue) => {
+    const upperArray = [...array]
+    const upperInValue = inValue.toUpperCase()
+
+    upperArray.map((char,index)=>{
+      upperArray[index] = char.toUpperCase()
+    })
+    // console.log(upperArray)
+    // console.log(inValue.toUpperCase())
+    if(upperArray.includes(upperInValue)){
+      return false
+    }
+    return true
+  }
   const handleKeyPress = (character) => {
     const indexes = []
     const arrayWord = word.split('')
-    toString()
+
+    console.log(character)
+    console.log(word)
+    
     arrayWord.map((char, index)=>{
       if(char.toUpperCase() === character.toUpperCase()) {
         indexes.push(index)
       }
     })
+    if(checkIsFail(arrayWord,character)) {
+      console.log('mal en component',arrayWord)
+      console.log(character)
+      updateFailList(character)
+    }
     updateWordBoard(indexes)
   }
 
@@ -110,14 +131,30 @@ function App() {
   const [wordBoard, setWordBoard]=useState([])
   const [word, setWord]=useState('')
   const[failTries, setFailTries]=useState([])
+  // useState TriesCount(word.length)
+  const[triesCount, setTriesCount]=useState(null)
+  /* useState checkIsOver() 
+      ↪ puede ser true(ganaste)
+        puede ser false(perdiste) */
+  const[checkIsOver, setCheckIsOver]=useState(null)
   
-  
+  const checkWinnerLoser = (boardToCheck) => {
+    if (!boardToCheck.includes(false)) {
+      // ganaste
+      return true
+    }
+    if (triesCount !== null && 
+        failTries.length === triesCount) {
+      // perdiste
+      return false
+    }
+    return null
+  }
 
   const updateFailList = (characterToAdd) =>{
     const newTries = [...failTries]
     newTries.push(characterToAdd)
     setFailTries(newTries)
-    // console.log('first',newTries)
   }
 
   const getRandom = () => {
@@ -128,6 +165,8 @@ function App() {
     setWordBoard(Array((WORDS_LIST[newRandomIndex].word).length).fill(false))
     setWord(WORDS_LIST[newRandomIndex].word)
     setFailTries([])
+    setTriesCount(WORDS_LIST[newRandomIndex].word.length)
+    setCheckIsOver(null)
 
     console.log('rand i ->', newRandomIndex)
     console.log('index word ->',indexWord)
@@ -135,21 +174,42 @@ function App() {
   }
 
   const updateWordBoard = (indexFounded) => {
+    if(checkIsOver != null) return
+
     const newWordBoard = [...wordBoard]
     indexFounded.map((_, index)=>{
       newWordBoard[indexFounded[index]] = true
     })
     setWordBoard(newWordBoard)
-  }
 
+    let count = triesCount
+    if(count != null) {
+      count = count-failTries.length
+    }
+    console.log(newWordBoard)
+    const gameStatus = checkWinnerLoser(newWordBoard)
+    if (gameStatus != null){
+      setCheckIsOver(gameStatus)
+      if(gameStatus === true){
+        // alert('you won')
+        setCheckIsOver(true)
+      }
+      if(gameStatus === false){
+        // alert('you lose')
+        setCheckIsOver(false)
+
+      }
+    }
+  }
+  // TODO: CheckWinner(wordBoard)
   return(
-    <section className='board-game-section'>
-      <h1>Guess the Word</h1>
-      <span>{indexWord ? WORDS_LIST[indexWord].clue : indexWord}</span>
-      {/* <span>{ WORDS_LIST[indexWord].clue }</span> */}
-      <section className='game-answer'>
+    <main>  
+      <section className='board-game-section'>
+        <h1>Guess the Word</h1>
+        <span>{indexWord ? WORDS_LIST[indexWord].clue :   indexWord}</span>
+        <section className='game-answer'>
         {
-          wordBoard.map((character, index) => {
+          wordBoard.map((_, index) => {
             return(
               <span 
                 key={index} >
@@ -158,162 +218,52 @@ function App() {
             )
           })
         }
-      </section>
-      
-      <section className='game-status'>
-        {/* {
-          failTries.length > 0 ?
-          failTries.map((char,index)=>{
-            <p key={index} >{char}</p>
-          }) : <p>you are doing well 🧐</p> 
-        } */}
-        <p><strong>FAILS ➡ </strong>{
-          failTries
-        }</p>
-       {/* tries   -   mistakes */}
-      </section>
-
-      <section className='game-word-inputs-container' >
-        {/* character inputs */}
-        {
-          indexWord ? 
-          <Input
-            word={word}
-            updateWordBoard={updateWordBoard}
-            updateFailList={updateFailList}
-            failTries={failTries}
-            // addMistake={checkMistake}
-            // {...console.log(wordBoard)}
-          /> 
-          : ''
-        }
-      </section>
-
-      <section className='game-options'>
-        <button onClick={getRandom} >{ indexWord ? 'Restart' : 'Start Game'}</button>
-        {/* random   -   reset */}
+        </section>
         
+        <section className='game-status'>
+          <p><strong> FAILS ➡ </strong>{failTries}</p>
+        </section>
+
+        <section className='game-word-input-container' >
+          {
+            indexWord ? 
+            <Input
+              word={word}
+              updateWordBoard={updateWordBoard}
+              updateFailList={updateFailList}
+              failTries={failTries}
+            /> 
+            : ''
+          }
+        </section>
+
+        <section className='game-options'>
+          <button onClick={getRandom} >{ indexWord ? 'Restart' :  'Start Game'}</button>
+        </section>
       </section>
-    </section>
+      {
+        checkIsOver !== null && (
+          <section className='game-over'>
+            <div className="text">
+              <h2>
+                {
+                  checkIsOver === false
+                    ? 'you spend your tries 😥'
+                    : 'you are right 😎'
+                }
+              </h2>
+
+              <footer>
+                <button onClick={getRandom} >
+                  Play Again{' ▶'}
+                  </button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
+    </main>
   )
 }
 
 export default App
-
-// const [mapa, setMapa] = useState([])
-//   const [palabra, setPalabra] = useState('')
-//   const [estado,setEstado] = useState(false)
-
-//   const getRandomInt = (max) => {
-//     return Math.floor(Math.random() * max)
-//   }
-
-//   const newRandom = () => {
-//     setPalabra(WORDS_LIST[getRandomInt(WORDS_LIST.length)].word)
-//     setMapa(Array(palabra.length).fill(false))
-//     console.log(mapa)
-//     console.log(palabra)
-//     console.log(estado)
-//   }
-
-//   const updateMapa = (index) => {
-//     const newMapa = [...mapa]
-//     newMapa[index] = estado
-//     setMapa(newMapa)
-//   }
-
-//   const comparar = (value) => {
-//     const palabraArray = palabra.split('')
-//     for (let index = 0; index < palabraArray.length; index++) {
-//       const element = palabraArray[index];
-//       console.log(value)
-//       console.log(element)
-//       if (element == value) {
-//         setEstado(true)
-//         updateMapa(index)
-//         console.log(mapa)
-//         console.log(index)
-//       }else{
-//         setEstado(false)
-//         console.log(mapa)
-//       }
-//     }
-//   }
-
-
-
-// mandar mapa como estado y palabra[index] como valor
-
-// palabra.split('').map((char,index) => {
-//   return(
-//     <Letra key={index} estadoLetra={mapa[index]} valor={char} >
-//     </Letra>
-//   )
-// })
-
-
-{/* <input type="text" onKeyUp={(event)=>comparar(event.target.value)} /> */}
-
-
-// const [newRandom, setNewRandom] = useState('')
-//   const [isFilled, setIsFilled] = useState(false)
-//   const [inputsArray, setInputsArray] = useState([''])
-
-//   const getRandomInt = (max) => {
-//     return Math.floor(Math.random() * max)
-//   }
-
-//   const handleKeyPress = (event,index) => {
-//     console.log(isFilled)
-//     console.log(newRandom[index])
-//   }
-
-//   const randomWord = () => {
-
-//     setNewRandom(WORDS_LIST[getRandomInt(WORDS_LIST.length)].word)
-//     // console.log(newRandom)
-//     // console.log(getRandomInt(WORDS_LIST.length))
-
-//     const inputsList = newRandom.split('')
-//     inputsList.map((character, index) => {
-//       inputsList[index] = character
-//     })
-//     setInputsArray(inputsList)
-//     console.log(inputsList)
-//   }
-
-//   return (
-//     <section className='board-game-section'>
-//       <h1>Word Scramble</h1>
-//       <section className="game-answer">
-//       
-//       {
-
-//       }
-//       </section>
-//       <section className='game-status'>
-//         <p><strong>tries</strong></p>
-//         <p><strong>mistakes</strong></p>
-//       {/* tries   -   mistakes */}
-//       </section>
-//       <section className='game-word-inputs-container' >
-//         {/* character inputs */}
-//         {
-//           inputsArray.map((character, index) => (
-//             <input /*className={isGuessed?'true':'false'}*/ 
-//             key={index} 
-//             type='text' 
-//             onKeyUp={(event) => handleKeyPress(event,index)} >
-//               {/* onKeyDownCapture */}
-//             </input>
-//           ))
-//         }
-//       </section>
-
-//       <section className='game-options'>
-//         <button onClick={randomWord} >Random</button>
-//         <button>Restart</button>
-//       {/* random   -   reset */}
-//       </section>
-//     </section>
-//   )
